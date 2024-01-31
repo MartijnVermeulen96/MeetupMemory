8e2400f54f7d7e42cf51006dd3d98983bdf09f47
martijnv <martijn.vermeulen@adyen.com>
2024-01-31 16:12:35 +0100

    Set boot_mallocs.sh example to use 4 cpus everywhere

--- scripts/boot_mallocs.sh
+++ scripts/boot_mallocs.sh
@@ -5,9 +5,9 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 
 echo "Spinning up some Docker containers"
 
-docker run --rm -d -e THREAD_MODE=single --name "Meetup_Single" --cpus 2 -m 2g meetup
-docker run --rm -d -e THREAD_MODE=single -e MALLOC_MAX_ARENA=2 --name "Meetup_Single_MALLOC_MAX_ARENA-2" --cpus 2 -m 2g meetup
-docker run --rm -d -e THREAD_MODE=multi --name "Meetup_Multi" --cpus 2 -m 2g meetup
-docker run --rm -d -e THREAD_MODE=multi -e MALLOC_MAX_ARENA=2 --name "Meetup_Multi_MALLOC_MAX_ARENA-2" --cpus 2 -m 2g meetup
+docker run --rm -d -e THREAD_MODE=single --name "Meetup_Single" --cpus 4 -m 2g meetup
+docker run --rm -d -e THREAD_MODE=single -e MALLOC_MAX_ARENA=2 --name "Meetup_Single_MALLOC_MAX_ARENA-2" --cpus 4 -m 2g meetup
+docker run --rm -d -e THREAD_MODE=multi --name "Meetup_Multi" --cpus 4 -m 2g meetup
+docker run --rm -d -e THREAD_MODE=multi -e MALLOC_MAX_ARENA=2 --name "Meetup_Multi_MALLOC_MAX_ARENA-2" --cpus 4 -m 2g meetup
 
 python "${SCRIPTPATH}/plot_container_memory.py"
